2c4b74b7c538ee1ac8eacaae3291f7ac0f1a3d7f
shilpa.rani <shilpa.rani@boeing.com>
2019-01-15 17:00:58 +0530

    Initial commit

---
+++ src/main/java/com/stackroute/Main.java
@@ -0,0 +1,48 @@
+package com.stackroute;
+
+import com.stackroute.domain.Actor;
+import com.stackroute.domain.Movie;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionReader;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+
+public class Main {
+    public static void main(String[] args){
+        //Movie movie=new Movie();
+
+
+        BeanFactory beanFactory=new XmlBeanFactory(new ClassPathResource("bean.xml"));
+        Movie movie1=(Movie) beanFactory.getBean("movie");
+        System.out.println(movie1.getActor());
+
+
+        BeanDefinitionRegistry beanDefinitionRegistry=new DefaultListableBeanFactory();
+        BeanDefinitionReader beanDefinitionReader=new XmlBeanDefinitionReader(beanDefinitionRegistry);
+        beanDefinitionReader.loadBeanDefinitions(new ClassPathResource("bean.xml"));
+        Movie movie2=((DefaultListableBeanFactory) beanDefinitionRegistry).getBean(Movie.class);
+        System.out.println(movie2.getActor());
+
+        ApplicationContext context=new ClassPathXmlApplicationContext("bean.xml");
+        Movie movie3=context.getBean(Movie.class);
+        System.out.println(movie3.getActor());
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
+
---
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -0,0 +1,50 @@
+package com.stackroute.domain;
+
+public class Actor {
+    private String Name;
+    private String Gender;
+    private int Age;
+
+    public String getName() {
+        return Name;
+    }
+
+    Actor(){}
+
+    public Actor(String name, String gender, int age) {
+        Name = name;
+        Gender = gender;
+        Age = age;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Actor{" +
+                "Name='" + Name + '\'' +
+                ", Gender='" + Gender + '\'' +
+                ", Age=" + Age +
+                '}';
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String gender) {
+        Gender = gender;
+    }
+
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+
+}
